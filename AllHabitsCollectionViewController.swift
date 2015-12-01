75e95d274235e65e9aa04ccb0e7884bef691542e
Maggy Hillen <mahkt6@gmail.com>
2015-12-01 13:43:30 -0800

    Adds valid logout functionality

--- AllHabitsCollectionViewController.swift
+++ AllHabitsCollectionViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class AllHabitsCollectionViewController: UICollectionViewController {
     
     @IBOutlet weak var levelSelector: UISegmentedControl!
--- Happy Habits/PopoverMenu.swift
+++ Happy Habits/PopoverMenu.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 import Popover
+import ChameleonFramework
+import QuartzCore
+import Parse
 
 var popover: Popover!
 var popoverOptions: [PopoverOption] = [
@@ -15,15 +18,26 @@ var popoverOptions: [PopoverOption] = [
     .BlackOverlayColor(UIColor(white: 0.0, alpha: 0.6))
 ]
 var popoverMenuItems = ["Logout", "Close"]
+var popoverColors = ColorSchemeOf(ColorScheme.Complementary, color: UIColor.flatSkyBlueColor(), isFlatScheme: true)
 
 func showPopoverMenu(viewcontroller: UIViewController) {
-        let tableView = UITableView(frame: CGRect(x: 0, y: 0, width: viewcontroller.view.frame.width / 2, height: 135))
-        tableView.delegate = viewcontroller
-        tableView.dataSource = viewcontroller
-        tableView.scrollEnabled = false
-        let point = CGPoint(x: viewcontroller.view.frame.width - 30, y: 55)
-        popover = Popover(options: popoverOptions, showHandler: nil, dismissHandler: nil)
-        popover.show(tableView, point: point)
+    let tableView = UITableView(frame: CGRect(x: 0, y: 0, width: viewcontroller.view.frame.width / 2, height: 85))
+    tableView.delegate = viewcontroller
+    tableView.dataSource = viewcontroller
+    tableView.scrollEnabled = false
+    tableView.backgroundColor = UIColor.flatSkyBlueColor()
+    tableView.separatorColor = UIColor.flatSkyBlueColorDark()
+    if tableView.respondsToSelector("setSeparatorInset:") {
+        tableView.separatorInset = UIEdgeInsetsZero;
+    }
+    if tableView.respondsToSelector("setLayoutMargins:") {
+        tableView.layoutMargins = UIEdgeInsetsZero;
+    }
+    
+    let point = CGPoint(x: viewcontroller.view.frame.width - 30, y: 55)
+    popover = Popover(options: popoverOptions, showHandler: nil, dismissHandler: nil)
+    popover.tintColor = UIColor.flatSkyBlueColor()
+    popover.show(tableView, point: point)
 }
 
 
@@ -32,7 +46,10 @@ extension UIViewController: UITableViewDelegate {
     public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         switch popoverMenuItems[indexPath.row] {
         case "Logout":
-            popover.dismiss()
+            PFUser.logOut()
+            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main",bundle: nil)
+            let destViewController = mainStoryboard.instantiateInitialViewController()
+            UIApplication.sharedApplication().keyWindow?.rootViewController = destViewController
             break
         case "Close":
             popover.dismiss()
@@ -40,7 +57,6 @@ extension UIViewController: UITableViewDelegate {
         default:
             break
         }
-        
     }
 }
 
@@ -53,6 +69,17 @@ extension UIViewController: UITableViewDataSource {
     public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
         cell.textLabel?.text = popoverMenuItems[indexPath.row]
+        cell.textLabel?.textAlignment = .Center
+        cell.textLabel?.font = loraFont.fontWithSize(16)
+        cell.textLabel?.textColor = UIColor.whiteColor()
+        cell.backgroundColor = UIColor.flatSkyBlueColor()
+        cell.selectionStyle = .None
+        if cell.respondsToSelector("setSeparatorInset:") {
+            cell.separatorInset = UIEdgeInsetsZero;
+        }
+        if cell.respondsToSelector("setLayoutMargins:") {
+            cell.layoutMargins = UIEdgeInsetsZero;
+        }
         return cell
     }
 }
